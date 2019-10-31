3984a1296a253727ca9b1a9a6679f8e4e536edf3
Andrew P. Sabelhaus <apsabelhaus@berkeley.edu>
2019-10-31 17:04:12 -0400

    CAD files for all figures in the IDETC paper (parameter definitions of the different models.)

--- simulations/helix_length.m
+++ simulations/helix_length.m
@@ -1,23 +1,24 @@
-% length_simulation_helix.m
-% Copyright 2015 Andrew P. Sabelhaus, Kyle Zampaglione
+% helix_length.m
+% Copyright 2015-2019 Andrew P. Sabelhaus, Kyle Zampaglione
 % This function calculates the length of a helix, as per the equations
-% in the ASME IDETC 2016 paper submission
+% in the various paper submissions
 % Inputs:
 % rail_length: the length of the rail of the helix (aka, arc length in 3D).
 %   This value also corresponds to the rest length of the actuator.
 % radius: the radius of the helix
-% rotation: the angle which the helix has been twisted. In this work, we
-%   refer to one rotation as 180 degrees. Also, this must be a scalar.
+% rotation: the angle which the helix has been twisted. 
+%   **UNITS ARE revolutions. 
+%   previously, was "one rotation as 180 degrees."
 % Output: length, the final length (aka, height) of the helix.
 
-function length = length_simulation_helix(rail_length, radius, num_rotations)
+function length = helix_length(rail_length, radius, rotation)
 
-% Convert to radians
-theta = num_rotations * pi;
+% Convert to radians. 2*pi rad / 1 rev.
+theta = rotation * 2 * pi;
 length = theta * sqrt((rail_length/theta)^2 - radius^2);
 
 % A correction for the case where there is no rotation:
-if num_rotations == 0
+if rotation == 0
     length = rail_length;
 end
 
