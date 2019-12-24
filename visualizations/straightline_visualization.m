6ca1c205c150aa71d1dafce963fe3ae651f12058
Andrew Sabelhaus <apsabelhaus@berkeley.edu>
2019-12-24 15:41:20 -0500

    revised the visualization for the straightline actuator. axes and figure dimensions still need tweaking

--- visualizations/straightline_visualization.m
+++ visualizations/straightline_visualization.m
@@ -1,6 +1,6 @@
 % straightline_visualization.m
 % Straight-line-edge actuator visualization
-% Copyright 2015 Kyle Zampaglione, Andrew P. Sabelhaus
+% Copyright 2015-2019 Kyle Zampaglione, Andrew P. Sabelhaus
 % 2.11.15
 % Calculates change in z height for twisting of a rope-ladder style helix
 % connected with rigid (straight-line) edges.
@@ -11,47 +11,94 @@
 
 clc; clear all; close all;
 
-%% VALUES STORED THROUGHOUT MULTIPLE ITERATIONS 
-z_totaled = [];
-angle_out = [];
-% an index into the above vectors
-count = 1;
+% Add the paths to the modeling equations
+addpath( '../analytical_models' );
 
 %% CONSTANTS FOR THE SIMULATION
 % The rungs have a set width. This is also the width between the two
 % edges of the helix.
-rungwidth = 2;
+w = 1;
+% The initial length of the helix (also the length of a rail)
+L0 = 10;
+% Number of rungs
+N = 11;
 % absolute distance between rungs. This value can also be interpreted as 
 % the length of the edge of the structure between two successive rungs.
-rungspacing = 2;  
-number_rungs = 10; 
+% ell_i = 1;  
+ell_i = L0 / (N-1);
+
 % Path to store the images that are generated
 save_path_base = 'img/straightline_visualization';
+
+% Range of input angles to evaluate.
+% Let's do it in terms of 'one rotation equals pi degrees.'
+% Maximum rotation angle:
+max_rot = 9 * pi;
+% choosing the number of rotations to be an interval of N+1 of the maximum
+% rotations makes the visualization easier: even numbers.
+num_rot = 10;
+% The range of input rotations to visualize, in radians, is
+rotations = linspace(0, max_rot, num_rot);
+
+% Store the length of the actuator for each rotation.
+L = zeros(size(rotations));
+
+% For plotting, some parameters on color and thickness.
+rungcolor = 'k';
+rungthickness = 2;
+railcolor = 'b';
+railthickness = 2;
+
+
+%% VALUES STORED THROUGHOUT MULTIPLE ITERATIONS 
+% z_totaled = [];
+L_history = [];
+angle_out = [];
+% an index into the above vectors
+count = 1;
+
+
 
 %% SIMULATION
 % j is the number of rotations to simulate. 
 % For example, j=1 is 180 degrees, j=3 is 540 degrees, etc.
-for j = 1:9
+
+% Here, just iterate over the number of rotations
+%for j = 1:9
+for j=1:size(rotations,2)
     %SETUP ============================================
     FigureHandle = figure;
     hold on
     set(gca,'FontSize',14);
     % Set the size of the figure window
-    set(FigureHandle,'Position',[100,100,150,300]);
+    % Vertical:
+%     set(FigureHandle,'Position',[100,100,150,300]);
     % Set the size of the figure window for printing (saving the fig)
-    set(FigureHandle,'PaperPosition',[2,2,3,4]);
+%     set(FigureHandle,'PaperPosition',[2,2,3,4]);
     % Label the axes
-    xlabel('  X');
-    ylabel('Y   ');
+%     xlabel('  X');
+%     ylabel('Y   ');
     %zlabel('Z');
     
-    zlim([0,18]);
-    twistangle =  j*180; %set twist angle in degrees. This is the total angle of twist for the entire structure.
-    angle_out(count) = twistangle; 
+    % For the horizontal actuator, the x-axis is length.
+    % and y-z are the radial and polar coordinates
+    xlabel('Length');
+    ylabel('Y');
+    zlabel('Z');
+    
+    title('Straight-Line Actuator Visualization');
+    
+    % Vertical:
+%     zlim([0,11]);
+
+    % Horizontal:
+    xlim([0, 11]);
+    zlim([-1, 1]);
+    ylim([-2, 2]);
     %==================================================
     
-    %Calculate angle of rotation between each rung
-    indivtwist = twistangle/(number_rungs-1);
+    % The angle between each rung is
+    theta_i = rotations(j) / (N-1);
     
     % Here, z refers to the z-axis location of the center of the rung.
     % x and y refer to the location of one outer point of a rung
@@ -60,24 +107,47 @@ for j = 1:9
     % One edge of the rung is then at (x, y, z) and the other is 
     % at (-x, -y, z).
 
-    % First rung is at height 0 and lies along the x-axis
-    x = 0; %initial x
-    y = rungwidth/2; %initial y
-    z_prev = 0; %initial z
-    plot3([-x,x],[-y,y], [0,0]);
+%     % First rung is at height 0 and lies along the x-axis
+%     x = 0; %initial x
+%     y = w/2; %initial y
+%     z_prev = 0; %initial z
+%     % 0-th rung
+%     plot3([-x,x],[-y,y], [0,0], rungcolor, 'LineWidth', rungthickness);
+    
+    % For the horizontal simulation, we'll do x as the length direction,
+    % and y-z as the other two.
+    z = 0; %initial z, one of the polar coordinates
+    y = w/2; %initial y, one of the radial coordinates
+    x_prev = 0; %initial x, length
+    % 0-th rung
+    plot3([x_prev,x_prev],[-y,y], [-z,z], rungcolor, 'LineWidth', rungthickness);
+    
 
-    hold on 
-
-    for i = 1:(number_rungs-1)
-        x_new = sind(indivtwist*i)*rungwidth/2; 
-        y_new = cosd(indivtwist*i)*rungwidth/2;
-        z = sqrt(rungspacing^2 - (x_new-x)^2 - (y_new-y)^2) + z_prev;
-        plot3([-x_new,x_new], [-y_new,y_new], [z,z])
-        plot3([x,x_new], [y,y_new],[z_prev,z],'g'); %connect rungs 
-        plot3([-x,-x_new], [-y,-y_new],[z_prev,z],'g'); %connect rungs 
-        x = x_new;
+    % Iterate over each rung and plot it
+    for i = 1:(N-1)
+%         x_new = sin(theta_i * i) * w/2;
+%         y_new = cos(theta_i * i) * w/2;
+        z_new = sin(theta_i * i) * w/2;
+        y_new = cos(theta_i * i) * w/2;
+%         z = sqrt(ell_i^2 - (x_new-x)^2 - (y_new-y)^2) + z_prev;
+        x = sqrt(ell_i^2 - (z_new-z)^2 - (y_new-y)^2) + x_prev;
+        
+        % plot the rung
+%         plot3([-x_new,x_new], [-y_new,y_new], [z,z], rungcolor, 'LineWidth', rungthickness);
+        plot3([x,x], [-y_new,y_new], [-z_new,z_new], rungcolor, 'LineWidth', rungthickness);
+        % plot the two rails
+%         plot3([x,x_new], [y,y_new],[z_prev,z],railcolor, 'LineWidth', railthickness); %connect rungs 
+        plot3([x_prev,x], [y,y_new],[z,z_new],railcolor, 'LineWidth', railthickness);
+%         plot3([-x,-x_new], [-y,-y_new],[z_prev,z], railcolor, 'LineWidth', railthickness); %connect rungs 
+        plot3([x_prev,x], [-y,-y_new],[-z,-z_new], railcolor, 'LineWidth', railthickness);
+        
+        % update for next iteration (next rung)
+%         x = x_new;
+%         y = y_new;
+%         z_prev = z;
+        z = z_new;
         y = y_new;
-        z_prev = z;
+        x_prev = x;
     end
 
     hold off 
@@ -87,15 +157,21 @@ for j = 1:9
 
     %legendtext = sprintf('Rung Width = %d ; Rung Spacing = %d',rungwidth, rungspacing );
     %legend(legendtext, 'Location','southoutside','Orientation','horizontal')
-    view(45,12) 
+    
+    % For the vertical"
+%     view(45,12) 
+    
+    % For the horizontal:
+%     view(18,18)
+    view(0,0);
     %print final z value to screen
     z_total = z;
     z_totaled(count) = z_total; 
     count = count+1;
     
     % Save this figure
-    save_fullpath_fig = strcat(save_path_base, int2str(twistangle), '.fig');
-    save_fullpath_eps = strcat(save_path_base, int2str(twistangle), '.eps');
+    save_fullpath_fig = strcat(save_path_base, int2str(theta_i), '.fig');
+    save_fullpath_eps = strcat(save_path_base, int2str(theta_i), '.eps');
     %savefig(save_fullpath_fig);
     %print(save_fullpath_eps,'-depsc')
 end
